7419d50d2680a3597266d919c29046ecda475021
henryheyhey92 <e0032134@u.nus.edu>
2021-08-21 15:41:14 +0800

    initial commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by henry lai on 21/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
